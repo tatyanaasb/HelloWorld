5cd923b4c0663c4fbf215fbcf1fa07e4943a15be
tatyanasb <btatyana45@gmail.com>
2020-06-11 19:02:21 -0400

    re-make, practice, re-read of all modules from wcci, first project on my pc

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is how we start this coding thing!");
+            Console.WriteLine(42);
+            Console.WriteLine(1.23);
+            Console.WriteLine(2 + 3);
+            Console.WriteLine("At first learning code was like playing with two magnets with the sides that dont magnetize but stay appart. " +
+                "\nThat energy you feel when you try to push them together was how it felt to try and push this new way of thinking (coding info) into my brain. " +
+                "\nI cant believe I finally understood.");
+            Console.WriteLine("Goodbye World!");
+
+            Console.ReadLine();
+        }
+    }
+}
