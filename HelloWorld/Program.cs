5d8cff5c9a744ec384676de8bd5540c5e412728f
tatyanasb <btatyana45@gmail.com>
2020-06-12 17:51:53 -0400

    Primitives. Strings.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,22 +2,102 @@
 using System.Runtime.CompilerServices;
 
 namespace HelloWorld
+
+//-------HelloWorld-------------------------------------------------------------------------------------------------
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//            Console.WriteLine("This is how we start this coding thing!");
+//            Console.WriteLine(42);
+//            Console.WriteLine(1.23);
+//            Console.WriteLine(2 + 3);
+//            Console.WriteLine("At first learning code was like playing with two magnets with the sides that dont magnetize but stay appart. " +
+//                "\nThat energy you feel when you try to push them together was how it felt to try and push this new way of thinking (coding info) into my brain. " +
+//                "\nI cant believe I finally understood.");
+//            Console.WriteLine("Goodbye World!");
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+//-------- PRIMITIVES ----------------------------------------------------------------------------------------------
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double doublevalue = 2.16440330489723961032;
+//            console.writeline(doublevalue);
+
+//            float floatvalue = 2.16440330489723961032f;
+//            console.writeline(floatvalue);
+
+//            console.readline();
+//        }
+//    }
+//}
+//-------- STRINGS -------------------------------------------------------------------------------------------------
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("This is how we start this coding thing!");
-            Console.WriteLine(42);
-            Console.WriteLine(1.23);
-            Console.WriteLine(2 + 3);
-            Console.WriteLine("At first learning code was like playing with two magnets with the sides that dont magnetize but stay appart. " +
-                "\nThat energy you feel when you try to push them together was how it felt to try and push this new way of thinking (coding info) into my brain. " +
-                "\nI cant believe I finally understood.");
-            Console.WriteLine("Goodbye World!");
+            //int numberOfSuits = 4;
+            //Console.WriteLine("I have " + numberOfSuits + " suits.");
+            //________________________________________
 
-            Console.ReadLine();
+            //int numberOfMaids = 7;
+            //int numberOfMops = 5;
+
+            //if (numberOfMaids == numberOfMops)
+            //{
+            //    Console.WriteLine("They might get it clear in half a year.");
+            //}
+
+            //String shoeDescription = "Their shoes were clean and neat";
+
+            //if (shoeDescription.Equals("Their shoes were clean and neat"))
+            //{
+            //    Console.WriteLine("This is odd, because, you know,");
+            //    Console.WriteLine("They hadn't any feet.");
+            //}_________________________________________
+
+            //string myGreeting = "Helllo";
+            //Console.WriteLine(myGreeting.ToUpper());
+            //Console.WriteLine(myGreeting.ToLower());
+            //Console.WriteLine(myGreeting);
+
+            //Console.WriteLine("Enter a color, please.");
+            //string response = Console.ReadLine();
+
+            //if(response.ToLower().Equals("pink"))
+            //{
+            //    Console.WriteLine("Pink roses are better than red roses.");
+            //}______________________________________________
+
+            //string palindrome = "A dog, a panic, in a pagoda";
+            //string whatsPalindrome = "I dont know what is the meaning of the first string";
+            //int palindromeLength = palindrome.Length;
+            //Console.WriteLine($"{palindrome} has {palindromeLength} characters.");
+            //Console.WriteLine($"{whatsPalindrome} has {whatsPalindrome.Length} characters.");
+
+            //if (palindrome.Length < whatsPalindrome.Length)
+            //{
+            //    Console.WriteLine($"{palindrome} has {whatsPalindrome.Length - palindrome.Length} less characters than {whatsPalindrome}");
+            //}
+            //if (whatsPalindrome.Length < palindrome.Length)
+            //{
+            //    Console.WriteLine($"{palindrome} has {whatsPalindrome.Length - palindrome.Length} more characters than {whatsPalindrome}");
+            //}_________________________________________________
+
+            //Console.WriteLine("Escape: \"This is an example of escaping\".");
+
+            //Console.WriteLine("Escaping, the use of special characters in strings. \nLike this new line");
+            //Console.WriteLine("\tAnd like this paragraph indent");
+
         }
     }
 }
